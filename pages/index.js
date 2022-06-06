cf9d5e8d29094b33b72a96cf2b838ae4507e99e6
github username <toniaroganti@gmail.com>
2022-06-05 20:33:14 -0400

    added sanity

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,8 @@ import Layout from '../components/Layout';
 import EventCard from '../components/parts/EventCard';
 import styles from '../styles/Home.module.css';
 import sanity from '../lib/sanity';
+import { motion } from 'framer-motion';
+import { TbArrowBottomCircle } from 'react-icons/tb';
 
 export default function Home({ events = [] }) {
 	console.log(events);
@@ -13,7 +15,17 @@ export default function Home({ events = [] }) {
 			<div className="container h-full pb-20">
 				<div className="flex h-screen w-full flex-col items-center justify-center space-y-3">
 					<h1 className="text-[#dfff94] text-3xl">Upcoming Events</h1>
+					<div className="animate-bounce text-green text-5xl">
+						<a href="#events" className="underline-none">
+							<TbArrowBottomCircle />
+						</a>
+					</div>
 				</div>
+				{/* <hr className="text-green opacity-50" />  */}
+				<h2 id="events" className="text-2xl text-center text-gray-light my-7">
+					Events
+				</h2>
+
 				<div className="container-md mx-auto grid grid-cols-1 md:grid-cols-2 gap-4">
 					{events &&
 						events?.map(({ title, info, time, date, image, location }) => (
