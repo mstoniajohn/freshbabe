0e77b47f7e2969a22ab8be1a995868ca219615a0
Tonia Roganti <toniaroganti@gmail.com>
2022-07-02 10:27:56 -0400

    social

--- components/Layout.js
+++ components/Layout.js
@@ -1,6 +1,7 @@
 import Head from 'next/head';
 import Image from 'next/image';
 import React from 'react';
+import { TbBrandInstagram } from 'react-icons/tb';
 import { ToastContainer } from 'react-toastify';
 import Header from './Header';
 
@@ -19,18 +20,38 @@ const Layout = ({ children, title }) => {
 			<ToastContainer />
 
 			<main className="bg-[#000000] h-full z-0">{children}</main>
-			<footer className=" bg-[#000000] text-center pt-40">
-				<a
-					href="https://www.toniaroganti.com/"
-					target="_blank"
-					rel="noopener noreferrer"
-					className="text-gray mx-auto mb-10"
-				>
-					Site created by Tonia Roganti
-					<span>
-						<Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-					</span>
-				</a>
+			<footer className="bg-[#000000] text-center pt-40 flex flex-col space-y-4 justify-center items-center">
+				<div className="text-gray ">
+					{/* social */}
+					<a
+						href="https://www.instagram.com/the.freshbabe/"
+						target="_blank"
+						rel="noopener noreferrer"
+						className=" flex items-center justify-center space-x-1"
+					>
+						<TbBrandInstagram className="text-3xl text-green" />{' '}
+						<span className="text-green-200">@the.freshbabe</span>
+					</a>
+					&copy; All Rights Reserved. 2022
+				</div>
+				<div>
+					<a
+						href="https://www.toniaroganti.com/"
+						target="_blank"
+						rel="noopener noreferrer"
+						className="text-gray-dark mt-4"
+					>
+						Site created by Tonia Roganti
+						<span>
+							<Image
+								src="/vercel.svg"
+								alt="Vercel Logo"
+								width={72}
+								height={16}
+							/>
+						</span>
+					</a>
+				</div>
 			</footer>
 		</div>
 	);
